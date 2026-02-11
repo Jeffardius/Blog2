f42b431d4d37231f1c36fc842f30932c9912afd6
Jeffardius <124376379+Jeffardius@users.noreply.github.com>
2026-02-11 00:55:50 +0000

    Auto-commit: 2026-02-11 00:55:50

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-// Dynamic content loader with embedded content
-document.addEventListener('DOMContentLoaded', function() {
-    // Embedded content from txt files
-    const contentData = {
-        'introduction-content': {
-            static: 'Why schould schools care?',
-            dynamic: 'There are many reasons why schools should take cybersecurity seriously. If the school gets DDOSed then no work depending on internet conectivity will be done. The information of students being leaked is very bad for public perception. If the infromation of staff member gets leaked then the chances of someone wanting to work at the school in question go down. If an account of a student/staff member gets hacked then the account can be used to cause further damage. Including but not limited to: full access to all information on students and staff, changing the code behind related websites, and damage to hardware.'
-        },
-        'confirmation-bias-content': {
-            text: 'Most children don\'t even know how networking works. How could they possibly do anything listed above? Cybersecurity is about stoping bad actors regardless of age from interfering from the daily operation.\n\nA strong password will stop any threat.\nAs computers grow more and more powerfull with every passing year it has become more viable to use "brute force password cracking". Which is the act is breaking passwords by guessing the password. Which is why mesures like passkeys and 2 factor athentication have bcome more populare in recent years.\n\nCybersecurity costs more then it\'s worth.\nDamage to even 5 SSDs is more money then the price of 1 cybersecurity auditor highered to prevent such damage.\n\nThe school I worked for never got hacked so I shouldn\'t care.\nEverything that can go wron will go wrong given enough time.'
-        },
-        'optimism-bias-content': {
-            text: '1) Never relly on one security solution. It is like trying to prevent a bank robbery by having only locked doors.\n2) Do not use obscure software. The more obscure the software is the more likely it has security issues the developers are unaware of.\n3) Train school staff to not trust any emails (or any other forms of communication) by default.\n4) Reduce the software footprint. Software may have flaws that allow for normal user to run in admin mode and as a result download malware.\n5) Do not provide devices/users with more rights then the bare minimum to get work done.\n6) Isolate devices from each other on the network based on who needs access to them. Such that student computers can not interact with server computers.\n7) Do not connect devices to the network unless they need it for operation. Printers that are intended for the secretary to use should not need internet access if it is already connected to the pc via usb.\n8) Password protect the BIOS/EUFI of devices. This will prevent bad actors from being able to boot an OS from a usb drive which cercomvents most secretary mesures.'
-        },
-        'examples-content': {
-            text: 'Yet another exmaple of how a optimism bias could effect actions is an employee storing all work related passwords in an unencrypted txt file in the desktop folder named passwords.txt. When asked the employee responds with "I am not even a server admin what could possibly go wrong if I get hacked". Unaware that the account(s) will be used to send out phishing emails or probe for other holes in the protections inplace. Such as checking for accounts being given more rights then needed. Leading to a lot of time waisted for the cyber security department.'
-        },
-        'solutions-content': {
-            text: 'The best way to deal with any bias in cyber security that can scew the judgement of an expert in the field is to remember the war between security and hacking is not a match of league of legends. It is a continues never ending war. Not a one of event totally isolated form the rest of the world. Undescovered hardware vualnrabilities may be inside all the hardware the organization makes use of.\n\nA war has many fronts and just beacuse the software front is "100% secure" does not mean every front is. Physical security is also a factor as what a bad actor with physical acess to a machine skyrockets. Humans are not perfect cretures and we all make mistakes eventually. A server admin acidentally forgetting to disable the root account in a l/unix server could be disasterous as the root user has FULL UNIMITGATED access to the entire system.\n\nThis is why to reduce the strnaglehold that biases can have over your choices in the workplace one should:\n1) Not to be afraid of being wrong.\n2) Avoid partaking in/creating echo chambers.\n3) Should be ok with concepts they are familuar with becoming outdated/deprecated.\n4) Be open to other\'s perspectives/solutions.\n5) Design systems with human error in mind.\n6) Go of tests not just past experiance.\n7) Seak out constructive & good fath arguments with peopl of oposing/conflicting views.\n8) Stay in touch with cyber security news.\n9) Inform others on how them lacking security effects others in the workplace.\n10) Treat no source as the objective truth. The turth often has hidden conditions for it to be "true".\n11) Humans are not without flaws and thus can not make flawless things.'
-        },
-        'sources-content': {
-            text: 'Sources:\nCrouse, M. (n.d.). Software makers encouraged to stop using C/C++ by 2026. techrepublic.\nhttps://www.techrepublic.com/article/cisa-fbi-memory-safety-recommendations/\n\nAlnifie, K. M., & Kim, C. (2023, February 23). Appraising the manifestation of optimism bias and its impact on human perception of cyber security a meta analysis. SCIRP. https://www.scirp.org/journal/paperinformation?paperid=123196\n\nBertrand. (2025, November 5). Neuroscience and cybersecurity: Understanding cognitive biases. Hardis Group.\nhttps://www.hardis-group.com/en/blog/neuroscience-and-cybersecurity-defeating-cognitive-biases-to-reduce-human-error/\n\nEling, M., & Jung, K. (2025). Optimism bias and its impact on cyber risk management decisions. ScienceDirect.\nhttps://www.sciencedirect.com/science/article/pii/S2950629824000018'
-        }
+// Dynamic content loader from txt files
+document.addEventListener('DOMContentLoaded', async function() {
+    // Static first text for introduction
+    const staticIntroductionText = 'Why schould schools care?';
+    
+    // Mapping of content containers to their txt files
+    const contentFileMapping = {
+        'introduction-content': '1.txt',
+        'confirmation-bias-content': '3.txt',
+        'optimism-bias-content': '4.txt',
+        'examples-content': '5.txt',
+        'solutions-content': '6.txt',
+        'sources-content': '7.txt'
     };
 
     // Fixed image files for each section
@@ -32,40 +22,52 @@ document.addEventListener('DOMContentLoaded', function() {
         'solutions-content': '6.png'
     };
 
-    // Load content for each section
-    for (const [containerId, content] of Object.entries(contentData)) {
-        const container = document.getElementById(containerId);
-        if (!container) continue;
+    // Function to fetch text file content
+    async function fetchTextFile(filename) {
+        try {
+            const response = await fetch(filename);
+            if (!response.ok) {
+                throw new Error(`Failed to load ${filename}`);
+            }
+            return await response.text();
+        } catch (error) {
+            console.error(`Error loading ${filename}:`, error);
+            return '';
+        }
+    }
 
+    // Load content for each section
+    for (const [containerId, filename] of Object.entries(contentFileMapping)) {
+        const container = document.getElementById(containerId);
         const section = container.closest('section');
-        const header = section ? section.querySelector('h2') : null;
+        const header = section.querySelector('h2');
 
         // Clear the "Loading content..." text
         container.innerHTML = '';
 
-        // For introduction-content, add both static and dynamic text
-        if (containerId === 'introduction-content' && content.static && content.dynamic) {
+        // Fetch the text file content
+        const fileContent = await fetchTextFile(filename);
+
+        // For introduction-content, add the static first text
+        if (containerId === 'introduction-content') {
             const staticText = document.createElement('div');
             staticText.className = 'loaded-text';
-            staticText.innerHTML = `<p>${content.static}</p>`;
+            staticText.innerHTML = `<p>${staticIntroductionText}</p>`;
             container.appendChild(staticText);
+        }
 
-            const dynamicText = document.createElement('div');
-            dynamicText.className = 'loaded-text';
-            const formattedText = content.dynamic.replace(/\n/g, '<br>');
-            dynamicText.innerHTML = `<p>${formattedText}</p>`;
-            container.appendChild(dynamicText);
-        } else if (content.text) {
-            // For all other sections, add the text
+        // Add the dynamically loaded text
+        if (fileContent) {
             const textElement = document.createElement('div');
             textElement.className = 'loaded-text';
-            const formattedText = content.text.replace(/\n/g, '<br>');
+            // Convert newlines to <br> tags for proper formatting
+            const formattedText = fileContent.replace(/\n/g, '<br>').trim();
             textElement.innerHTML = `<p>${formattedText}</p>`;
             container.appendChild(textElement);
         }
 
         // Add image if this section has one
-        if (sectionImages[containerId] && header) {
+        if (sectionImages[containerId]) {
             const imgContainer = document.createElement('div');
             imgContainer.className = 'image-container';
             imgContainer.innerHTML = `
